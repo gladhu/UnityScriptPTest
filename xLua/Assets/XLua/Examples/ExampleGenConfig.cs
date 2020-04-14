5f26de83766f7dfca4a6386a586084c4f3191620
gladhu <gladhu@tencent.com>
2020-04-14 19:06:05 +0800

    【XLua】 XLua升级到最新版本

--- xLua/Assets/XLua/Examples/ExampleGenConfig.cs
+++ xLua/Assets/XLua/Examples/ExampleGenConfig.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using XLua;
+//using System.Reflection;
+//using System.Linq;
 
 //配置的详细介绍请看Doc下《XLua的配置.doc》
 public static class ExampleGenConfig
@@ -29,9 +31,9 @@ public static class ExampleGenConfig
                 typeof(Ray2D),
                 typeof(Time),
                 typeof(GameObject),
-                typeof(Transform),
                 typeof(Component),
                 typeof(Behaviour),
+                typeof(Transform),
                 typeof(Resources),
                 typeof(TextAsset),
                 typeof(Keyframe),
@@ -42,6 +44,8 @@ public static class ExampleGenConfig
                 typeof(SkinnedMeshRenderer),
                 typeof(Renderer),
                 typeof(WWW),
+                typeof(Light),
+                typeof(Mathf),
                 typeof(System.Collections.Generic.List<int>),
                 typeof(Action<string>),
                 typeof(UnityEngine.Debug)
@@ -62,11 +66,18 @@ public static class ExampleGenConfig
     //黑名单
     [BlackList]
     public static List<List<string>> BlackList = new List<List<string>>()  {
+                new List<string>(){"System.Xml.XmlNodeList", "ItemOf"},
                 new List<string>(){"UnityEngine.WWW", "movie"},
+    #if UNITY_WEBGL
+                new List<string>(){"UnityEngine.WWW", "threadPriority"},
+    #endif
                 new List<string>(){"UnityEngine.Texture2D", "alphaIsTransparency"},
                 new List<string>(){"UnityEngine.Security", "GetChainOfTrustValue"},
                 new List<string>(){"UnityEngine.CanvasRenderer", "onRequestRebuild"},
                 new List<string>(){"UnityEngine.Light", "areaSize"},
+                new List<string>(){"UnityEngine.Light", "lightmapBakeType"},
+                new List<string>(){"UnityEngine.WWW", "MovieTexture"},
+                new List<string>(){"UnityEngine.WWW", "GetMovieTexture"},
                 new List<string>(){"UnityEngine.AnimatorOverrideController", "PerformOverrideClipListCleanup"},
     #if !UNITY_WEBPLAYER
                 new List<string>(){"UnityEngine.Application", "ExternalEval"},
